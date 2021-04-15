9d15397a9327180e191b6319681e2c957fe74fcd
Juan Jose Vargas Fletes <androidapps0703@gmail.com>
2021-04-14 20:18:26 -0600

    Initial structure for REST API with Java

---
+++ api_java/src/test/java/com/xtream/api/XtreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtream.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
