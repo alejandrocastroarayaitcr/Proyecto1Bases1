2a68707338f072f3abb49f65f31d3a8ab6b37cc1
Juan Jose Vargas Fletes <androidapps0703@gmail.com>
2021-04-21 13:16:22 -0600

    Added Hibernate session factory in order to call SP in the generic class

--- api_java/src/main/java/com/xtream/api/Repositories/SPInvoker.java
+++ api_java/src/main/java/com/xtream/api/Repositories/SPInvoker.java
@@ -1,25 +1,49 @@
 package com.xtream.api.Repositories;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.xtream.api.HibernateUtil;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
-public abstract class  SPInvoker<Model> {
+public abstract class SPInvoker<Model> {
+    protected SessionFactory sessionFactory;
+
     private String procedureName;
     private String[] procedureParams;
 
-    private final String callStatement = "{call %s (%s)}";
+    private Query spQuery;
+    private String callStatement = "call %s (%s)";
 
-    @Autowired
     public SPInvoker(String procedureName, String[] procedureParams){
         this.procedureName = procedureName;
         this.procedureParams = procedureParams;
+
+        spQuery = getFormattedSQL();
+
+        sessionFactory = HibernateUtil.getSessionFactory();
     }
 
     // Execute the store procedure
     public abstract Model execute();
 
-    // Joins the the sp name and params to the call statement into a string
-    protected String getSPSQLSentence(){
-        String params = String.join(", ", procedureParams);
-        return String.format(callStatement, procedureName, params);
+    // This will set the params for the SQL
+    protected <Type> void setParam(String param, Type value){
+        spQuery.setParameter(param, value);
+    }
+
+    // This will format the SQL with the SP name and its params
+    private Query getFormattedSQL(){
+        formatSPParams();
+
+        String params = String.join(",", procedureParams);
+        callStatement = String.format(callStatement, procedureName, params);
+
+        return sessionFactory.openSession().createQuery(callStatement);
+    }
+
+    // This will format the params
+    private void formatSPParams(){
+        for (int i = 0; i < procedureParams.length; i++){
+            procedureParams[i] = ":" + procedureParams[i];
+        }
     }
 }
