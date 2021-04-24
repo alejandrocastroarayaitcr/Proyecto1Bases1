e8364df30202897eaadce9f22b28e8cc2ab80c24
Juan Jose Vargas Fletes <androidapps0703@gmail.com>
2021-04-23 20:38:33 -0600

    Added some basic component style on CSS

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,8 +1,14 @@
-import './index.css';
-
 function App() {
   return (
     <div>
+      <header className="grid">
+        <h1 className="header-logo">XTream</h1>
+      </header>
+      <div className="container grid">
+        <p>Esto es un texto de prueba</p>
+        <p>Esto es un texto de prueba</p>
+        <p>Esto es un texto de prueba</p>
+      </div>
     </div>
   );
 }
