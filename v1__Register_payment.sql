d06b08ae21beccebf6ab3557e1dcc5b8cf32f466
alejandrocastroarayaitcr <70798923+alejandrocastroarayaitcr@users.noreply.github.com>
2021-05-03 20:04:02 -0600

    Changed syntaxis

--- v1__Register_payment.sql
+++ v1__Register_payment.sql
@@ -14,28 +14,16 @@ CREATE PROCEDURE registrar_pago(
     IN pComputerName VARCHAR(45),
     IN pIPAddress VARCHAR(45),
     IN pTransactionType VARCHAR(45),
+    IN pTransactionSubType VARCHAR(45),
     IN transaccion_anterior bit
 )
 BEGIN
 
- DECLARE exit handler for SQLEXCEPTION
-BEGIN
- if @inicie_transaccion = 1 and transaccion_anterior = 0 then
-	ROLLBACK;
- end if;
-  GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, 
-  @errno = MYSQL_ERRNO, @text = MESSAGE_TEXT;
- SET @full_error = CONCAT("ERROR ", @errno, " (", @sqlstate, "): ", @text);
- SELECT @full_error as mensaje_error;
- END;
-
 SET autocommit = 0;
 
 SET @usuario = (SELECT idUser from users where users.username = pSenderName);
-SELECT @usuario as usuario;
 
 SET @merchant = (SELECT idMerchants from merchants where merchants.name = pMerchant);
-SELECT @merchant as merchant;
 
 SET @inicie_transaccion = 0;
 if @inicie_transaccion = 0 and transaccion_anterior = 0 then
@@ -47,12 +35,11 @@ INSERT INTO paymentStatus(name)
 VALUES('In progress');
 
 SET @payment_status = (SELECT idPaymentStatus from paymentStatus where paymentStatus.idPaymentStatus = last_insert_id());
-SELECT @payment_status as payment_status;
 
 INSERT INTO paymentAttempts(postTime,amount,currencySymbol,referenceNumber,errorNumber,merchantTransactionNumber,description,paymentTimeStamp,computerName,ipAddress,checksum,idUser,idmerchants,idpaymentStatus)
 VALUES(current_time(),pAmount,pCurrencySymbol,999999999999*RAND(),999999999999*RAND(),999999999999*RAND(),pDescription,current_time(),pComputerName,pIPAddress,sha1(concat(@usuario,current_time(),char(round(rand()*25)+97))),@usuario,@merchant,@payment_status);
 
-CALL registrar_transaccion(pSenderName,pMerchant,pAmount,pCurrencySymbol,pDescription,pXTreamPercentage,pComputerName,pIPAddress,pTransactionType,1);
+CALL registrar_transaccion(pSenderName,pMerchant,pAmount,pCurrencySymbol,pDescription,pXTreamPercentage,pComputerName,pIPAddress,pTransactionType,pTransactionSubType,1);
 
 if @inicie_transaccion = 1 and transaccion_anterior = 0 then
 	COMMIT;
@@ -61,4 +48,4 @@ end if;
 END $$
 DELIMITER ;
 
-CALL registrar_pago('julioprofetv','PayPal',20.0,'$','this is a transaction',5.0,'AlePC','127.0.0.1','subscription',0)
+ CALL registrar_pago('ale123','PayPal',20.0,'$','this is a transaction',5.0,'AlePC','127.0.0.1','subscription','tier one subscription',0)
