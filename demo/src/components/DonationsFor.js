48fd8e35909404384bff812d1cd53bc8fa40bd51
alejandrocastroarayaitcr <alejandrocastroaraya@estudiantec.cr>
2021-05-10 05:38:31 -0600

    Now reads input and uses it as query param for url

--- demo/src/components/DonationsFor.js
+++ demo/src/components/DonationsFor.js
@@ -2,20 +2,22 @@ import axios from 'axios'
 import {useEffect, useState } from 'react'
 import DonationsItems from './DonationsItems'
 
-const url = "http://localhost:3000/donations_for_channel?channelName=Florentino"
+const url = "http://localhost:3000/donations_for_channel?channelName="
+var queryParam = ""
 
 function DonationsFor(props){
     const [donationsFor, setDonationsFor] = useState("")
 
-    const inputChange = e => {
-        setDonationsFor(e.target.value);
+    const inputChange = (e) => {
+        queryParam = e.target.value
+        setDonationsFor(queryParam)
 
     }
 
     const fetchData = (e) => {
         e.preventDefault()
 
-        axios.get(url)
+        axios.get(url+queryParam)
         .then((response)=>{
             setDonationsFor(response.data)
             console.log(response.data)
@@ -28,23 +30,19 @@ function DonationsFor(props){
     return (
         <div className="donations-for-container">
             <p className="title"><strong>Ver todas las donaciones para el canal: </strong></p>
-            <input type = "text" value = {donationsFor} onChange={inputChange} placeholder = "Nombre del canal" />
+            <input type = "text" value = {donationsFor} onChange={(e) => inputChange(e)} placeholder = "Nombre del canal" />
             <button className = "fetch-button" onClick={(e) => fetchData(e)}>
                 Submit 
             </button>
 
             <div className="donationsFor">
-        {donationsFor &&
+        {donationsFor && Array.isArray(donationsFor) &&
           donationsFor.map((donation, index) => {
 
             return (
               <div className="donation" key={index}>
-                <h3>Donation {index + 1}</h3>
-                <h2>{donation.ChannelName}</h2>
-
-                <div className="details">
-                  <p>📖: {donation.SumDonationsReceived} </p>
-                </div>
+                <h2>{"Nombre de canal: " + donation.ChannelName}</h2>
+                <h2>{"Suma total de todas sus donaciones: " + donation.SumDonationsReceived}</h2>
               </div>
             );
           })}
